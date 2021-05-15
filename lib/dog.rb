5d2c4a928ab1a516e6d5ba558f3b9b820463dd01
Shasha Yan <shashayan@gmail.com>
2021-05-15 23:46:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog
+
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all = [ ]
+    @@all << self.new
+  end
+  
+  def self.print_call 
+    @@all.each do 
+      puts name
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+end
